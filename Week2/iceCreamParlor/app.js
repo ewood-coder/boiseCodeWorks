0d0e3f5ecf590b3b480844851dbc70e94cf116ba
Emma Wood <emmawood.coder@gmail.com>
2024-03-19 15:08:17 -0600

    updated some sections

---
+++ Week2/iceCreamParlor/app.js
@@ -0,0 +1,31 @@
+// const iceCream = [{
+// 	name: 'Cookie Dough',
+// 	price: 1.25,
+// 	quantity: 0
+// },
+// {
+// 	name: 'Vanilla',
+// 	price: 1,
+// 	quantity: 0
+// },
+// {
+// 	name: 'Strawberry',
+// 	price: 1.25,
+// 	quantity: 0
+// }],
+
+// const toppings = [{
+// 	name: 'Sprinkles',
+// 	quantity: 0,
+// 	price: .25
+// },
+// {
+// 	name: 'Chocolate Chips',
+// 	price: .25,
+// 	quantity: 0
+// },
+// {
+// 	name: 'Cookie Chunks',
+// 	price: .5,
+// 	quantity: 0
+// }]
