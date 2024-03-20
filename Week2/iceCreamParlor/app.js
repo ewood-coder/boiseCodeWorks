d2b7b8788a487fb3bd00f8235ebee10b0d09445f
Emma Wood <emmawood.coder@gmail.com>
2024-03-19 19:06:24 -0600

    more updates

--- Week2/iceCreamParlor/app.js
+++ Week2/iceCreamParlor/app.js
@@ -1,31 +1,47 @@
-// const iceCream = [{
-// 	name: 'Cookie Dough',
-// 	price: 1.25,
-// 	quantity: 0
-// },
-// {
-// 	name: 'Vanilla',
-// 	price: 1,
-// 	quantity: 0
-// },
-// {
-// 	name: 'Strawberry',
-// 	price: 1.25,
-// 	quantity: 0
-// }],
-
-// const toppings = [{
-// 	name: 'Sprinkles',
-// 	quantity: 0,
-// 	price: .25
-// },
-// {
-// 	name: 'Chocolate Chips',
-// 	price: .25,
-// 	quantity: 0
-// },
-// {
-// 	name: 'Cookie Chunks',
-// 	price: .5,
-// 	quantity: 0
-// }]
+const iceCream = [{
+	name: 'Chocolate',
+	price: 4,
+	quantity: 0
+},
+{
+	name: 'Vanilla',
+	price: 3,
+	quantity: 0
+},
+{
+	name: 'Strawberry',
+	price: 4,
+	quantity: 0
+}]
+//********************************/
+const toppings = [{
+	name: 'Sprinkles',
+	price: 2,
+	quantity: 0
+},
+{
+	name: 'Chocolate Chips',
+	price: 3,
+	quantity: 0
+},
+{
+	name: 'Gummy Worms',
+	price: 2,
+	quantity: 0
+}]
+//********************************/
+const cones = [{
+	name: 'Waffle Cone',
+	price: 3,
+	quantity: 0
+},
+{
+	name: 'Waffle Bowl',
+	price: 5,
+	quantity: 0
+},
+{
+	name: 'Dipped Cone',
+	price: 4,
+	quantity: 0
+}]
