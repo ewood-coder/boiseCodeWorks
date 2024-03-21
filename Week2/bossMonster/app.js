d30b5634d557be027c981c2599197a915c166b82
Emma Wood <emmawood.coder@gmail.com>
2024-03-21 16:24:58 -0600

    damage hero func()

--- Week2/bossMonster/app.js
+++ Week2/bossMonster/app.js
@@ -25,11 +25,11 @@ const boss = {
 
 
 
-function attackBoss() {
+function damageBoss() {
 	let teamDamage = 0
 	// let bossHealth = boss.health
 
-	let attackTotal = heroes.forEach(hero => {
+	heroes.forEach(hero => {
 		teamDamage += hero.damage
 	})
 
@@ -40,4 +40,61 @@ function attackBoss() {
 	}
 	console.log('Damage to Boss:', teamDamage)
 	console.log('Boss Health:', boss.health)
+
+
 }
+
+function damageHeroes() {
+	// let eachHero = heroes.forEach(hero => {
+	// 	hero.health
+	// 	let heroHealth = calcDamage(hero)
+	// })
+
+	let hero1 = heroes[0].health
+	let hero2 = heroes[1].health
+
+	if (boss.damage >= hero1) {
+		hero1 = 0
+	} else if (boss.damage < hero1) {
+		hero1 -= boss.damage
+	}
+
+	if (boss.damage >= hero2) {
+		hero2 = 0
+	} else if (boss.damage < hero2) {
+		hero2 -= boss.damage
+	}
+
+	console.log('Hero 1 Health:', hero1)
+	console.log('Hero 2 Health:', hero2)
+	console.log('Damage to Heroes:', boss.damage)
+
+}
+
+// function calcDamage(hero) {
+
+// 	let attackStrength = hero.health -= boss.damage
+// 	if (boss.damage >= attackStrength) {
+// 		attackStrength = 0
+// 	} else if (boss.damage < attackStrength) {
+// 		attackStrength -= boss.damage
+
+// 	}
+// 	console.log('Hero Health:', attackStrength)
+// 	console.log('Damage to Heroes:', boss.damage)
+// }
+
+// function damageHeroes() {
+
+// 	heroes.forEach(hero => {
+// 		let heroHealth = hero.health -= boss.damage
+// 		if (boss.damage >= heroHealth) {
+// 			heroHealth = 0
+// 		} else if (boss.damage < heroHealth) {
+// 			heroHealth -= boss.damage
+
+// 		}
+// 		console.log('Hero Health:', heroHealth)
+// 		console.log('Damage to Heroes:', boss.damage)
+// 	})
+// }
