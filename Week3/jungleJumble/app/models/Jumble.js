1625ae78b2ee69f7145a337d87cbd2d841310b2a
Emma Wood <emmawood.coder@gmail.com>
2024-03-28 17:32:19 -0600

    added form

--- Week3/jungleJumble/app/models/Jumble.js
+++ Week3/jungleJumble/app/models/Jumble.js
@@ -20,9 +20,9 @@ export class Jumble {
 
 	get ListTemplate() { // a basic list template to get drawing
 		return `
-			<button class="col-2 my-3 fs-5 startButton">start</button>
-			<span class="col-5 my-3">${this.name}</span>
-			<span class="col-5 my-3">⏲️ ${this.fastestTime}s</span>
+			<button class="col-2 my-2 fs-5 startButton">start</button>
+			<span class="col-5 my-2">${this.name}</span>
+			<span class="col-5 my-2">⏲️ ${this.fastestTime}s</span>
 		`
 	}
 
