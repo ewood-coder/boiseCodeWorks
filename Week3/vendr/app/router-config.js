11e06051ba8a771ec8165fa066c5bfb805a7f83a
Emma Wood <emmawood.coder@gmail.com>
2024-03-28 10:48:14 -0600

    please work

--- Week3/vendr/app/router-config.js
+++ Week3/vendr/app/router-config.js
@@ -4,6 +4,20 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
 	{
 		path: '',
+		controllers: [],
+		view: /*html*/`
+			<div class="bg-white p-3">
+				<div class="card-body p-4">
+				<p>Home Page</p>
+				<h1> Welcome to Vendr</h1>
+				<a href="#/snacks" class="btn pinkColor">Go To Menu</a>
+				</div>
+			</div>
+		`
+	},
+
+	{
+		path: '#/snacks',
 		controllers: [SnacksController],
 		view: 'app/views/SnacksView.html'
 	},
