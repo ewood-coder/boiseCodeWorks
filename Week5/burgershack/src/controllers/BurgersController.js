183d3a750bf5b4f6944093205ad6b183f48d9cdb
Emma Wood <emmawood.coder@gmail.com>
2024-04-08 16:52:35 -0600

    Finished all requirements except for update items (was told it would be shown later in the week)

--- Week5/burgershack/src/controllers/BurgersController.js
+++ Week5/burgershack/src/controllers/BurgersController.js
@@ -5,7 +5,66 @@ import BaseController from "../utils/BaseController.js";
 export class BurgersController extends BaseController {
 	constructor () {
 		super('api/burgers');
-		this.router
-		//...
+
+		// NOTE: You can place multiple on the same this.router or separate them ex/
+		// this.router
+		// 	.get('', this.testBurgers)
+		// 	.get('', this.getBurgers)
+
+		this.router.get('/test', this.testBurgers)
+		this.router.get('', this.getBurgers)
+		this.router.post('', this.createBurger)
+		this.router.delete('/:burgerId', this.deleteBurger)
+	}
+
+	testBurgers(request, response, next) {
+		console.log('🍔🍔🍔🍔🍔')
+		response.send('🍔 I got burgers for ya 🍔')
+	}
+
+
+
+	/**
+	* Sends all values back to the client
+	* @param {import("express").Request} request
+	* @param {import("express").Response} response
+	* @param {import("express").NextFunction} next
+	*/
+
+	async getBurgers(request, response, next) {
+		try {
+			const burgers = await burgersService.getBurgers()
+			response.send(burgers) // then send that info back to the client
+		}
+		catch (error) {
+			next(error) // next pushes then onto the next door, then the hall, until they are out of "next" options, which sends them home (usually with an error)
+			console.error(error)
+		}
+	}
+
+
+	async createBurger(request, response, next) {
+		try {
+			const burgerData = request.body
+			const burger = await burgersService.createBurger(burgerData)
+			console.log("CREATING BURGER 🔨🍔")
+			response.send(burger)
+		}
+		catch (error) {
+			next(error)
+		}
+	}
+
+
+	async deleteBurger(request, response, next) {
+		try {
+			const burgerId = request.params.burgerId
+			await burgersService.deleteBurger(burgerId)
+			console.log("❌ Burger was deleted ❌")
+			response.send("❌ Burger was deleted ❌")
+		}
+		catch (error) {
+			next(error)
+		}
 	}
 }
--- Week5/burgershack/src/services/BurgersService.js
+++ Week5/burgershack/src/services/BurgersService.js
@@ -1,11 +1,27 @@
 import { dbContext } from "../db/DbContext.js"
 
 
-
 class BurgersService {
 
+	async getBurgers() {
+		const burgers = await dbContext.Burgers.find()
+		return burgers
+	}
 
 
+	async createBurger(burgerData) {
+		const burger = await dbContext.Burgers.create(burgerData)
+		return burger
+	}
+
+
+	async deleteBurger(burgerId) {
+		const burgerToDelete = await dbContext.Burgers.findById(burgerId)
+
+		if (!burgerToDelete) throw new Error(`Sorry, that burger was already deleted. Burger ID: ${burgerId}`)
+
+		await dbContext.Burgers.deleteOne({ _id: burgerId })
+	}
 }
 
 export const burgersService = new BurgersService()
