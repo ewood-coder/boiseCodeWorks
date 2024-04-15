b494f6a426bcdcb7ac1f73190916028376e2cb60
Emma Wood <emmawood.coder@gmail.com>
2024-04-15 12:56:56 -0600

    initialize

---
+++ Week6/vueMiner/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+const appState = computed(() => AppState)
+
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+   <footer class="bg-dark text-light">
+    Made with 💖 by CodeWorks
+  </footer>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root{
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
---
+++ Week6/vueMiner/src/pages/AccountPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+
+const account = computed(() => AppState.account)
+
+</script>
+
+<template>
+  <div class="about text-center">
+    <div v-if="account">
+      <h1>Welcome {{ account.name }}</h1>
+      <img class="rounded" :src="account.picture" alt="" />
+      <p>{{ account.email }}</p>
+    </div>
+    <div v-else>
+      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  max-width: 100px;
+}
+</style>
