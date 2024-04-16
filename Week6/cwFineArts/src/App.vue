175ea476bd2732bd703b84c825a03d9c05299699
Emma Wood <emmawood.coder@gmail.com>
2024-04-16 14:25:22 -0600

    update

--- Week6/cwFineArts/src/App.vue
+++ Week6/cwFineArts/src/App.vue
@@ -1,33 +1,52 @@
 <script setup>
+import "./assets/scss/main.scss";
 import { AppState } from './AppState'
 import Navbar from './components/Navbar.vue'
 
 
 </script>
 
-<template>
-  <header>
-    <Navbar />
-  </header>
-  <main>
-    <router-view />
-  </main>
-  <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
+<template class="bgColor">
+	<header>
+		<Navbar />
+	</header>
+	<main>
+
+		<div class="container-fluid">
+
+			<section class="row flex-column">
+				<div>
+					<i class="mdi mdi-book-open-blank-variant-outline"></i>
+					<p>1 of 27</p>
+				</div>
+				<div>
+					<button class="my-2">Next</button>
+					<button class="my-2">Previous</button>
+				</div>
+
+			</section>
+
+			<section class="row flex-column">
+				<div>i have content</div>
+			</section>
+
+		</div>
+
+	</main>
 </template>
 
 <style lang="scss">
 @import "./assets/scss/main.scss";
 
 :root {
-  --main-height: calc(100vh - 32px - 64px);
+	--main-height: calc(100vh - 32px - 64px);
 }
 
+.newFont {
+	font-family: "Playfair Display", serif;
+}
 
-footer {
-  display: grid;
-  place-content: center;
-  height: 32px;
+.bgColor {
+	background-color: #D9D9D9;
 }
 </style>
--- Week6/cwFineArts/src/components/Navbar.vue
+++ Week6/cwFineArts/src/components/Navbar.vue
@@ -4,68 +4,81 @@ import { loadState, saveState } from '../utils/Store.js';
 import Login from './Login.vue';
 
 const theme = ref(loadState('theme') || 'light')
+const nav = ref(navbarColor)
 
 onMounted(() => {
-  document.documentElement.setAttribute('data-bs-theme', theme.value)
+	document.documentElement.setAttribute('data-bs-theme', theme.value)
 })
 
 function toggleTheme() {
-  theme.value = theme.value == 'light' ? 'dark' : 'light'
-  document.documentElement.setAttribute('data-bs-theme', theme.value)
-  saveState('theme', theme.value)
+	theme.value = theme.value == 'light' ? 'dark' : 'light'
+	document.documentElement.setAttribute('data-bs-theme', theme.value)
+	saveState('theme', theme.value)
+}
+
+function navbarColor() {
+	const nav = document.createElement("nav");
+	if (theme.value == 'dark') {
+		nav.classList.remove('borderBtm')
+		nav.classList.add('borderBtm2')
+	}
 }
 
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <div>
-        <button class="btn text-light" @click="toggleTheme"
-          :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
-          <i class="mdi" :class="theme == 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></i>
-        </button>
-      </div>
-      <Login />
-    </div>
-  </nav>
+	<nav class="navbar navbar-expand-sm px-3 borderBtm">
+		<router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+			<div class="d-flex flex-column align-items-center newFont fs-1 fw-bold">
+				CW
+			</div>
+		</router-link>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+			aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarText">
+			<!-- <ul class="navbar-nav me-auto">
+				<li>
+					<router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+						About
+					</router-link>
+				</li>
+			</ul> -->
+			<!-- LOGIN COMPONENT HERE -->
+			<div class="ms-auto">
+				<button class="btn text-dark fs-3 btn-success p-1 py-0 me-4" @click="toggleTheme"
+					:title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
+					<i class="mdi" :class="theme == 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></i>
+				</button>
+			</div>
+			<Login />
+		</div>
+	</nav>
 </template>
 
 <style scoped>
 a:hover {
-  text-decoration: none;
+	text-decoration: none;
 }
 
 .nav-link {
-  text-transform: uppercase;
+	text-transform: uppercase;
 }
 
 .navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
+	border-bottom: 2px solid var(--bs-success);
+	border-bottom-left-radius: 0;
+	border-bottom-right-radius: 0;
 }
 
 @media screen and (min-width: 576px) {
-  nav {
-    height: 64px;
-  }
+	nav {
+		height: 64px;
+	}
+}
+
+.borderBtm {
+	box-shadow: 0px 0px 5px 3px rgba(0, 0, 0, 0.5);
 }
 </style>
